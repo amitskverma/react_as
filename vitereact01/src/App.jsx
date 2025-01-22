845c256c4166a685c392ffbfc640a28a68394e26
Amit verma <190012191+amitskverma@users.noreply.github.com>
2025-01-22 17:56:26 +0530

    basic api call for nodejs enpoint

--- vitereact01/src/App.jsx
+++ vitereact01/src/App.jsx
@@ -1,21 +1,56 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
+
+import React, { useState, useEffect } from 'react';
 
 function App() {
- 
-  let [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
-  const decrement = () => {if(setCount>0)setCount(count - 1)};
-  const reset = () => setCount(0);
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    const fetchData = () => {
+      fetch('http://192.168.1.13:3000/get')
+        .then((response) => response.json())
+        .then((data) => setData(data))
+        .catch((error) => console.error('Error fetching data:', error));
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <>
-     <button onClick={increment}>Increment{count}</button>
-      <button onClick={decrement}>Decrement{count}</button>
-      <button onClick={reset}>Reset{count}</button>
-    </>
-  )
+    <div>
+      <h1>Data from API</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item._id}>
+            {/* Display relevant data from each item */}
+            {item.name} - {item.age} - {item.work} 
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+// export default MyComponent;
+
+
+
+// function App() {
+ 
+//   let [count, setCount] = useState(0);
+//   const increment = () => setCount(count + 1);
+//   const decrement = () => {if(setCount>0)setCount(count - 1)};
+//   const reset = () => setCount(0);
+
+
+//   return (
+//     <>
+//      <button onClick={increment}>Increment{count}</button>
+//       <button onClick={decrement}>Decrement{count}</button>
+//       <button onClick={reset}>Reset{count}</button>
+//     </>
+//   )
+// }
 
 export default App
--- vitereact01/vite.config.js
+++ vitereact01/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0' // This tells Vite to listen on all available network interfaces
+  }
+});
