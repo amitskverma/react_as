ba5572e1a5dcef1a5c3213aa11a246f5f52d26e6
amitskverma <amitskverma@hotmail.com>
2025-01-22 15:56:34 +0530

    react-project created using npx create-react-app

---
+++ basicreact01/src/App.js
@@ -0,0 +1,11 @@
+import Form01 from "./form";
+
+function App() {
+  return (
+    <div className="App">
+    <Form01 />
+    </div>
+  );
+}
+
+export default App;
---
+++ basicreact01/src/form.jsx
@@ -0,0 +1,13 @@
+function Form01(){
+    return (
+        <form>
+            <label>First Name</label>
+            <input type="text" />
+            <label>Last Name</label>
+            <input type="text" />
+            <button>Submit</button>
+        </form>
+    )
+}
+
+export default Form01
